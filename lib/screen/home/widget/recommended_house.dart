8c409a56967ad0769e6a59f9433d71cdbf13a8ad
Afif Akromi <massachusetts48@gmail.com>
2022-11-06 15:43:19 +0700

    finish detail house

--- lib/screen/home/widget/recommended_house.dart
+++ lib/screen/home/widget/recommended_house.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:house_rent/model/house.dart';
+import 'package:house_rent/screen/detail/detail.dart';
 import 'package:house_rent/widget/circular_icon.dart';
 
 class RecommendedHouse extends StatelessWidget {
@@ -14,77 +15,88 @@ class RecommendedHouse extends StatelessWidget {
       height: 340,
       child: ListView.separated(
           scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => Container(
-                height: 300,
-                width: 230,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(recommendedList[index].imageUrl),
-                          fit: BoxFit.cover,
+          itemBuilder: (context, index) => GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return DetailPage(house: recommendedList[index]);
+                      },
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 300,
+                  width: 230,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(recommendedList[index].imageUrl),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      right: 15,
-                      top: 15,
-                      child: CircularIconButton(
-                        iconUrl: 'assets/icons/mark.svg',
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: Container(
-                        color: Colors.white54,
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  recommendedList[index].name,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline1!
-                                      .copyWith(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  recommendedList[index].address,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1!
-                                      .copyWith(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                              ],
-                            ),
-                            CircularIconButton(
-                              iconUrl: 'assets/icons/mark.svg',
-                              color: Theme.of(context).colorScheme.secondary,
-                            )
-                          ],
+                      Positioned(
+                        right: 15,
+                        top: 15,
+                        child: CircularIconButton(
+                          iconUrl: 'assets/icons/mark.svg',
+                          color: Theme.of(context).colorScheme.secondary,
                         ),
                       ),
-                    )
-                  ],
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          color: Colors.white54,
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    recommendedList[index].name,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline1!
+                                        .copyWith(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    recommendedList[index].address,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyText1!
+                                        .copyWith(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                              CircularIconButton(
+                                iconUrl: 'assets/icons/mark.svg',
+                                color: Theme.of(context).colorScheme.secondary,
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
           separatorBuilder: (_, index) => const SizedBox(width: 20),
